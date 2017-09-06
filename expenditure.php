5427b70e034b7a14b6adb7baaeaf54ae848889f1
eq62Roket <otimtony>
2017-09-06 15:04:14 +0300

    php script for expenditure table

---
+++ expenditure.php
@@ -0,0 +1,25 @@
+<?php
+
+
+$usertotalSavingsExpenditure = $_POST['totalSavingsExpenditure'];
+$usertotalTransportExpenditure = $_POST['totalTransportExpenditure'];
+$usertotalMedicalExpenditure= $_POST['totalMedicalExpenditure'];
+$usertotalHomeneedsExpenditure = $_POST['totalHomeneedsExpenditure'];
+$usertotalEducationExpenditure= $_POST['totalEducationExpenditure'];
+$usertotalOthersExpenditure = $_POST['totalOthersExpenditure'];
+
+$user = "ivan@165.227.67.248";
+$pass = "m3tr0n3t";
+$host= "idev2.gcinnovate.com";
+$dbname="cashTime"
+
+$con = mysqli_connect($host,$user,$pass,$dbname);
+$sql="insert into Expenditure(totalSavingsExpenditure,totalTransportExpenditure,totalMedicalExpenditure,totalHomeneedsExpenditure,totalEducationExpenditure,totalOthersExpenditure) values('".$usertotalSavingsExpenditure."','".$usertotalTransportExpenditure."','".$usertotalMedicalExpenditure."','".$usertotalHomeneedsExpenditure."','".$usertotalEducationExpenditure."','".$usertotalOthersExpenditure."');";
+
+if ($con->query($sql) === TRUE) {
+    $last_id = $con->insert_id;
+    echo "" . $last_id;
+} else {
+    echo "Error: " . $sql . "<br>" . $con->error;
+}
+?>

a3729f14219acd8b5bccf03eb4e2bb08f7e9798b
eq62Roket <otimtony>
2017-09-06 15:06:22 +0300

    php script to update the goal table

---
+++ goal_update.php
@@ -0,0 +1,28 @@
+<?php
+
+
+$usergoalName = $_POST['goalName'];
+$usergoalAmount = $_POST['goalAmount'];
+$usergoalStartDate= $_POST['goalStartDate'];
+$usergoalEndDate = $_POST['goalEndDate'];
+$goal_id = $_POST['id'];
+
+$user = "ivan@165.227.67.248";
+$pass = "m3tr0n3t";
+$host= "idev2.gcinnovate.com";
+$dbname="cashTime"
+
+$con = mysqli_connect($host,$user,$pass,$dbname);
+$sql="update Goal
+set goalName = '".$usergoalName."',
+goalAmount = '".$usergoalAmount."',
+goalStartDate = '".$usergoalStartDate."',
+goalEndDate = '".$usergoalEndDate."'
+where goal_id = ".$goal_id;
+
+if ($con->query($sql) === TRUE)
+    echo true;
+else
+    echo false;
+
+?>
